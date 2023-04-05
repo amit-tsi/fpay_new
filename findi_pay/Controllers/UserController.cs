9c786f73dce3cb6c5a9868eab599a2772c8a26ad
Amit Tripathi <amit.tripathi@tsiplc.com>
2023-04-05 07:15:12 +0530

    05/04/2023

---
+++ findi_pay/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace findi_pay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SendMoney() { 
+            return View();
+        }
+        public ActionResult Refund()
+        {
+            return View();
+        }
+        public ActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
